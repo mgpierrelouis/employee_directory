a67d19ca42cb4c6036689b64e01af3122889bdbf
mgpierrelouis <mgpierrelouis@gmail.com>
2021-09-23 18:01:46 -0400

    set up app.js to render required components

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Table from './component/Table';
+import Wrapper from './component/Wrapper';
+import Header from './component/Header';
+
+function App() {
+    return (
+        <Wrapper>
+           <Header />
+           <Table />
+        </Wrapper>
+    );
+
+}
